ba926b91c531e04d0ce6ff8217b8f05d6deb1e36
Adedaramola Adetimehin <mailstokindle@gmail.com>
2023-10-10 15:05:13 +0100

    remove trait

--- src/Http/TermiiClient.php
+++ src/Http/TermiiClient.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Adedaramola\Termii\Http;
 
-use Adedaramola\Termii\Concerns\CanSendRequest;
 use Adedaramola\Termii\Contracts\TermiiClientContract;
 use Adedaramola\Termii\Enums\Method;
 use Adedaramola\Termii\Exceptions\TermiiException;
@@ -15,8 +14,6 @@ use Illuminate\Support\Facades\Http;
 
 class TermiiClient implements TermiiClientContract
 {
-    use CanSendRequest;
-
     public function __construct(
         protected readonly string $url,
         protected readonly string $apiKey,
