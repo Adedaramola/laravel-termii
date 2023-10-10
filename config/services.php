e5dc62f465b0ffbefc72696a11588ceeeca58a06
Adedaramola Adetimehin <mailstokindle@gmail.com>
2023-10-10 23:59:10 +0100

    fix config

--- config/services.php
+++ config/services.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 return [
     'termii' => [
         'url' => env('TERMII_URL', 'https://api.ng.termii.com/api'),
-        'token' => env('TERMII_API_KEY'),
+        'api_key' => env('TERMII_API_KEY'),
     ],
 ];
